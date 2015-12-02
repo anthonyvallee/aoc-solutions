3e444d0b6e65113a7e54b00787db65e0b5389284
anthonyvallee <tony.vallee@hotmail.com>
2015-12-02 15:00:09 -0500

    Solution complete

--- program.js
+++ program.js
@@ -1,12 +1,22 @@
-var fs = require('fs');
-
-var input = "";
-
-fs.readFile('./input.json', function(err, data) {
-  input = parseJSON(data);
-  console.log(input);
-});
+var fs          = require('fs');
+var q           = require('q'),
+    readFile    = q.nfbind(fs.readFile);
 
 function parseJSON(data) {
   return JSON.parse(data);
 }
+
+function count(object) {
+  var floor = object.floor;
+  var count = 0;
+  for (var i = 0; i < floor.length; ++i) {
+    (floor[i] == '(') ? count++ : count--;
+  }
+  return '' + count;
+}
+
+readFile('./input.json', 'utf-8')
+  .then(parseJSON)
+  .then(count)
+  .tap(console.log)
+  .done();
